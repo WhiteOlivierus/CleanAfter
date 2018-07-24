2043064f0bdfe6efd75f8b680f161fd69136db70
WhiteOlivierus <36532695+WhiteOlivierus@users.noreply.github.com>
2018-07-24 19:39:55 +0200

    Prefabs,scripts and scenes

---
+++ Assets/Scripts/CleanOmeter.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CleanOmeter : MonoBehaviour
+{
+    private Text amountClean;
+    private float initialDirt;
+    private GameObject cleanIndicator;
+    private GameObject gameOver;
+
+    void Start()
+    {
+        amountClean = GetComponent<Text>();
+        initialDirt = GetInteractables();
+        cleanIndicator = GameObject.Find("Clean");
+        gameOver = GameObject.Find("GameOver");
+        gameOver.SetActive(false);
+    }
+
+    void Update ()
+    {
+        float dirtLeft = GetInteractables();
+
+        float clean = Mathf.Round(100 - ((dirtLeft / initialDirt) * 100));
+
+        amountClean.text = (clean + "% Cleaned");
+
+        if(clean >= 98)
+        {
+            GameOver();
+        }
+    }
+
+    int GetInteractables()
+    {
+        return GameObject.FindGameObjectsWithTag("Interactable").Length;
+    }
+
+    void GameOver()
+    {
+        cleanIndicator.SetActive(false);
+        gameOver.SetActive(true);
+    }
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+            Application.Quit();
+    }
+
+    public void loadLevel(int sceneToLoad)
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+    private void Start()
+    {
+        Destroy(GameObject.Find("Tutorial"), 10f);
+    }
+}
